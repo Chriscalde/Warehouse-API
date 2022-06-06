c35761193b01981943a2f6441ced221ac07c2738
Christian Calderon <chris.calderon.ortega@gmail.com>
2022-06-06 11:05:34 -0600

    Database connected

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 
 const PORT = process.env.PORT || 8080
 const app = express();
+
+dotenv.config();
+connectDB();
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on localhost port ${PORT}`))
