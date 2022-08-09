e96ad2e19300f47d656b5ab4f08b65a21d2cbbbd
Christian Calderon <chris.calderon.ortega@gmail.com>
2022-08-09 14:33:03 -0600

    Now it works

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
+const bodyParser = require('body-parser');
 
 
 const PORT = process.env.PORT || 8080
