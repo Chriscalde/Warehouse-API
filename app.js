4c398fc8b972dfdb2691a6026a8667a77c37e980
Christian Calderon <chris.calderon.ortega@gmail.com>
2022-08-25 13:17:00 -0600

    Changed stuff

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ const cors = require('cors');
 const cors_proxy = require('cors-anywhere')
 const expresHttpProxy = require('express-http-proxy');
 
-const HOST = process.env.HOST || '0.0.0.0'
-const CORS_PORT = 8081
-const PORT = process.env.PORT || 8080
-const app = express();
+
 
 const userRouter = require('./routes/user.routes');
 const publicRouter = require('./routes/public.routes');
@@ -17,11 +14,24 @@ const jobRouter = require('./routes/job.routes');
 const orderRouter = require('./routes/order.routes');
 
 
+//DB connection
+dotenv.config();
+connectDB();
+
+
+const HOST = process.env.HOST || '0.0.0.0'
+const CORS_PORT = 8081
+const PORT = process.env.PORT || 8080
+const app = express();
+
 //This is to use the body requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+//Allows cors
+app.use(cors());
 
 //This is for the Router
 app.use('/user', userRouter);
@@ -29,24 +39,19 @@ app.use('/', publicRouter);
 app.use('/job', jobRouter);
 app.use('/order', orderRouter);
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
-}));
-//DB connection
-dotenv.config();
-connectDB();
+// app.use(cors({
+//     origin: '*',
+//     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+// }));
+// cors_proxy.createServer({
+//     originWhitelist: [],
+//     requireHeader: ['origin', 'x-requested-with'],
+//     removeHeaders: ['cookie', 'cookie2']
+// }).listen(CORS_PORT, HOST, function() {
+//     console.log('Running CORS Anywhere on ' + HOST + ':' + CORS_PORT)
+// });
 
-
-cors_proxy.createServer({
-    originWhitelist: [],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
-}).listen(CORS_PORT, HOST, function() {
-    console.log('Running CORS Anywhere on ' + HOST + ':' + CORS_PORT)
-});
-
-app.use(expresHttpProxy(`localhost:${CORS_PORT}`))
+// app.use(expresHttpProxy(`localhost:${CORS_PORT}`))
 
 
 
