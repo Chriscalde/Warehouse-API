b8e05270f1109def8953d6032c4b978c47f2a4bc
Christian Calderon <chris.calderon.ortega@gmail.com>
2022-08-29 10:15:00 -0600

    Added order routes

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const controller = require('../controllers/order.controller');
 
 router.post('/create', controller.createItem);
+router.get('/get/job', controller.getJobOrder);
 
 module.exports = router;
