e7b83b31c2e0d1dd6103e5ce389f761793d1e58c
Chris <chris.calderon.ortega@gmail.com>
2022-11-21 16:04:20 -0700

    Added template reader

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,4 +1,5 @@
 const Order = require('../models/order.model');
+const Job = require('../models/job.model');
 
 exports.createItem = async(req, res) => {
     const jobId = req.body.jobId
@@ -56,6 +57,70 @@ exports.getJobOrder = (req, res) => {
             res.status(200).json({
                 success: true,
                 data: order
+            })
+        }
+    })
+}
+
+exports.getJobNumber = (req,res) => {
+    const jobNo = req.body.jobNo
+    Job.findOne({jobNumber:jobNo},(err,job)=>{
+        console.log(job);
+        if(err){
+            res.status(500).send(err)
+        } else {
+            Order.find({jobId:job.id},(err,order)=>{
+                if(err){
+                    res.status(500).send(err)
+                }else{
+                    res.status(200).json({
+                        success: true,
+                        data: order
+                    })
+                }
+            })
+        }
+    })
+}
+
+exports.readTemplate = (req,res) => {
+    const jobNo = req.body.jobNo
+    const type = req.body.type
+    const code = req.body.code
+    const item = req.body.item
+    const purchaseOrder = req.body.purchaseOrder
+    const location = req.body.location
+    const description = req.body.description
+    const date = req.body.date
+    const status = req.body.status
+
+    
+
+    Job.findOne({jobNumber:jobNo},(err,job)=>{
+        if(err){
+            res.status(500).send(err)
+        } else {
+            const jobId = job.id
+            const newItem = new Order({
+                jobId,
+                type,
+                code,
+                item,
+                purchaseOrder,
+                location,
+                description,
+                date,
+                status
+            });
+            newItem.save(async(err,order)=>{
+                if(err){
+                    res.status(500).send(err)
+                } else {
+                    res.status(200).json({
+                        success: true,
+                        data: order
+                    })
+                }
             })
         }
     })
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/order.controller');
 router.post('/create', controller.createItem);
 router.get('/get/:id', controller.getJobOrder);
 router.get('/all', controller.getAllOrders);
-
+router.get('/jobNo',controller.getJobNumber);
+router.post('/read',controller.readTemplate);
 module.exports = router;
